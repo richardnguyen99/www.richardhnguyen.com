b2d9383a34bd428cdf1e80a49da654363d4a7e6f
Richard H Nguyen <richard@richardhnguyen.com>
2024-10-29 13:51:15 -0700

    feat: update not-found to use invalid pathname

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import * as React from "react";
-import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import ExternalLink from "@/components/external-link";
 
 export default function NotFoundBlogPost() {
+  const pathname = usePathname();
+
   return (
     <>
       <div
@@ -12,68 +18,21 @@ export default function NotFoundBlogPost() {
           "items-center justify-between lg:flex",
           "font-mono text-sm",
         )}
-      >
-        <p
-          className={cn(
-            "flex justify-center",
-            "w-full lg:w-auto",
-            "border-b border-gray-300 dark:border-neutral-800 lg:rounded-xl lg:border",
-            "dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:dark:bg-zinc-800/30",
-            "bg-gradient-to-b from-zinc-200",
-            "pb-6 pt-8 lg:p-4",
-          )}
-        >
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+      ></div>
+
+      <div>
+        <h1 className="text-6xl font-black">Oops. Not Found</h1>
+        <h3 className="mt-4 text-2xl font-bold">
+          Sorry, but the following path does not exist:{" "}
+          <code className="text-lime-400 dark:text-lime-500">{pathname}</code>
+        </h3>
+        <p className="mt-4">
+          If you believe this is a mistake, please{" "}
+          <ExternalLink href="https://github.com/richardnguyen99/next.richardhnguyen.com/issues/new">
+            open an issue on GitHub
+          </ExternalLink>
+          .
         </p>
-
-        <div
-          className={cn(
-            "fixed bottom-0 left-0",
-            "h-48 w-full lg:static lg:size-auto",
-            "flex items-end justify-center",
-            "bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:bg-none",
-          )}
-        >
-          <a
-            className={cn(
-              "pointer-events-none lg:pointer-events-auto",
-              "flex place-items-center gap-2",
-              "p-8 lg:p-0",
-            )}
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div
-        className={cn(
-          "relative z-[-1]",
-          "flex place-items-center",
-          "before:absolute before:h-[300px] before:w-full before:content-[''] sm:before:w-[480px] before:lg:h-[360px]",
-          "before:-translate-x-1/2 before:rounded-full",
-          "before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl",
-          "before:dark:bg-gradient-to-br before:dark:from-transparent",
-          "before:dark:to-blue-700 before:dark:opacity-10",
-          "after:absolute after:-z-20 after:h-[180px] after:w-full after:content-[''] sm:after:w-[240px]",
-          "after:bg-gradient-conic after:translate-x-1/3",
-          "after:from-sky-200 after:via-blue-200 after:blur-2xl",
-          "after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40",
-        )}
-      >
-        <h1 className="text-4xl font-black">Not Found</h1>
       </div>
 
       <div
@@ -83,77 +42,71 @@ export default function NotFoundBlogPost() {
           "text-center lg:text-left",
         )}
       >
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/blogs"
           className={cn(
             "group rounded-lg",
             "border border-transparent",
             "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
             "px-5 py-4 transition-colors",
           )}
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Blogs
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
 
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            What I have written
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        <Link
+          href="/projects"
           className={cn(
             "group rounded-lg",
             "border border-transparent",
             "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
             "px-5 py-4 transition-colors",
           )}
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
+            Projects
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
 
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Explore projects I have worked on
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
+          href="/gist"
           className={cn(
             "group rounded-lg",
             "border border-transparent",
             "px-5 py-4 transition-colors",
             "hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
           )}
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Gist
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
 
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Small stuff I have programmed
           </p>
-        </a>
+        </Link>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/richardnguyen99/next.richardhnguyen.com"
           className={cn(
             "group rounded-lg",
             "border border-transparent",
@@ -164,14 +117,14 @@ export default function NotFoundBlogPost() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            GitHub
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
 
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Don&apos;t know what&apos;s wrong? Go check my GitHub source
           </p>
         </a>
       </div>

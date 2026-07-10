e3d99604245065eb92f5eef384d47eafd6e648f1
Richard Nguyen <richard@richardhnguyen.com>
2026-07-10 13:08:08 -0700

    fix: fix changelog link in light mode

--- src/app/blogs/[slug]/components/changelog.tsx
+++ src/app/blogs/[slug]/components/changelog.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+
 import { Changelog as ChangelogType } from "../type";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronUp } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import ExternalLink from "@/components/external-link";
 
@@ -13,29 +14,9 @@ type Props = {
 
 export default function Changelog({ changelog }: Props): React.JSX.Element {
   const [collapsing, setCollapsing] = React.useState(true);
-  // return (
-  //   <div className="mx-(--article-gutter-size) mt-6 w-(--article-container-size) gap-4 border-t border-neutral-500 pt-6 dark:border-neutral-700">
-  //     <h4 className="text-base font-bold md:text-lg">Changelog</h4>
-  //     {changelog.map((commit) => (
-  //       <div key={commit.url}>
-  //         <a href={commit.url} target="_blank" rel="noopener noreferrer">
-  //           {commit.message}
-  //         </a>
-  //         <p>
-  //           {commit.date ? new Date(commit.date).toLocaleString() : "No date"}
-  //         </p>
-  //       </div>
-  //     ))}
-  //   </div>
 
-  // );
   return (
     <>
-      {/* <nav
-        className={cn(
-          "ease-curve-d duration-normal transition-toc pointer-events-auto mx-(--article-gutter-size) mt-6 flex h-0 w-(--article-container-size) translate-y-12.5 gap-4 overflow-visible border-t border-neutral-500 pt-6 opacity-100 xl:ml-8 xl:h-fit xl:w-[calc((100%-var(--article-container-size))/2-4rem)] xl:translate-y-16.5 xl:cursor-default xl:overflow-visible xl:border-none xl:bg-transparent dark:border-neutral-700",
-        )}
-      > */}
       <div className="mx-(--article-gutter-size) mt-6 flex w-(--article-container-size) gap-4 border-t border-neutral-500 pt-6 dark:border-neutral-700">
         <div className="w-full">
           <div className="relative mx-(--article-gutter-size) flex w-(--article-container-size) items-start justify-between overflow-hidden rounded-md xl:mx-[unset] xl:[display:unset] xl:w-[unset]">
@@ -56,10 +37,10 @@ export default function Changelog({ changelog }: Props): React.JSX.Element {
               <div className="">
                 <ScrollArea
                   className={cn(
-                    "ease-curve-d max-h-[calc(100vh-300px)] w-full overflow-auto rounded-md transition-[max-height] duration-300 xl:max-h-[calc(100vh-300px)] dark:bg-neutral-900",
+                    "ease-curve-d max-h-[calc(100vh-300px)] w-full overflow-auto rounded-md bg-neutral-100 transition-[max-height] duration-300 xl:max-h-[calc(100vh-300px)] dark:bg-neutral-900",
                     {
-                      "mb-4 h-fit": collapsing,
-                      "max-h-0 xl:max-h-0": !collapsing,
+                      "mb-4 h-fit": !collapsing,
+                      "max-h-0 xl:max-h-0": collapsing,
                     },
                   )}
                 >
@@ -86,7 +67,7 @@ export default function Changelog({ changelog }: Props): React.JSX.Element {
                           href={change.url}
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="[&_span]:text-white!"
+                          className="[&_span]:text-black! dark:[&_span]:text-white!"
                         >
                           {change.message}
                         </ExternalLink>
